cae2e15cbbcba5f7be08e5150f66bfcad951cec6
Sonu Kumar <sonu3660@gmail.com>
2024-03-23 13:06:20 +0530

    Implement recipe details dynamically

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrl: './recipe-list.component.css'
+  styleUrl: './recipe-list.component.css',
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    // new keyword is used to call constructor
+    new Recipe(
+      'A Test recipe',
+      'This is simply test',
+      'https://realfood.tesco.com/media/images/472x310-Tesco-DinnerFor2-FridgeRaidFeasts-16024-BaconMushroomBreakfastHash-0e5cf25f-f42c-45a2-b4a3-f4f9f06ee002-0-472x310.jpg'
+    ),
+  ];
 }
